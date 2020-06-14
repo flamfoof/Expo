008aa13803e97b33b759369c1cbb2dc54aaf7b98
Felix Lam <felixlam0316@gmail.com>
2020-06-13 22:28:40 -0400

    - Exhibit and Auditorium assets imported

---
+++ Assets/Scripts/Video/YoutubeVidPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using YoutubeExtractor;
+
+public class YoutubeVidPlayer : MonoBehaviour
+{
+    public string url;
+    public int quality;
+
+    public async void Run()
+    {
+        IEnumerable<VideoInfo> videoInfos = await DownloadUrlResolver.GetDownloadUrlsAsync(url);
+        VideoInfo video = videoInfos.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == quality);
+
+        if(video.RequiresDecryption)
+        {
+            DownloadUrlResolver.DecryptDownloadUrl(video);
+        }
+
+        //GetComponent<MediaPlayerCtrl>().m_strFileName = video.DownloadUrl;
+        GetComponent<UnityEngine.Video.VideoPlayer>().url = video.DownloadUrl;
+    }
+}

77fd8dc8f183dd817fdd2cb7de7439eb1c6513c7
Felix Lam <felixlam0316@gmail.com>
2020-06-17 01:40:40 -0400

    - Added Dev build and Release build to the WebGL build tool and cleaned up the code

--- Assets/Editor/WebGLBuild.cs
+++ Assets/Editor/WebGLBuild.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System;
 using UnityEngine;
 using UnityEditor;
-using System.Diagnostics;
 
 public class WebGLBuild : MonoBehaviour
 {
@@ -18,85 +17,104 @@ public class WebGLBuild : MonoBehaviour
     static string photonDir = "Assets/StoreAssets/Photon";
     static string photonVoiceDir = "PhotonVoice";
     static string unphotonVoiceDir = "Exclude~";
+    static BuildTarget buildTarget = BuildTarget.WebGL;
+    static BuildOptions buildOpt = BuildOptions.Development;
 
     //Change the number of variables to anything, as long as it
     //increases the length to the scene count, which is currently 3 of them
-    static string[] levels = {"1", "2", "3"};
+    static string[] levels = {"1"};
 
-    [MenuItem("WebGLBuilder/Make Build")]
+    [MenuItem("WebGLBuilder/Make Release Build")]
+    static void ReleaseBuild()
+    {
+        buildOpt = BuildOptions.None;
+        CreateBuild();
+    }
+
+    [MenuItem("WebGLBuilder/Make Dev Build")]
+    static void DevBuild()
+    {
+        buildOpt = BuildOptions.Development;
+        CreateBuild();
+    }
+
     static void CreateBuild()
     {
-        string excludeFolder = "";
         string excludeFolderPath = "";
         UsefulShortcuts.ClearConsole();
 
         //before build
         if(!Directory.Exists(photonDir + "/" + unphotonVoiceDir) || true)
         {
-            UnityEngine.Debug.Log("Target Directory not created");
-            //excludeFolder = AssetDatabase.CreateFolder(photonDir, unphotonVoiceDir);
-            //excludeFolderPath = AssetDatabase.GUIDToAssetPath(excludeFolder);
+            Debug.Log("Target Directory not created");
             excludeFolderPath = photonDir + "/" + unphotonVoiceDir;
-            UnityEngine.Debug.Log("Created a folder: " + excludeFolderPath);
-            UnityEngine.Debug.Log("Moving: " + AssetDatabase.MoveAsset(photonDir + "/" + photonVoiceDir, excludeFolderPath));
-        } else{
-            UnityEngine.Debug.Log("Unable to create folder directory");
+            Debug.Log("Created a folder: " + excludeFolderPath);
+            try
+            {
+                Debug.Log("Moving: " + AssetDatabase.MoveAsset(photonDir + "/" + photonVoiceDir, excludeFolderPath));
+            } 
+            catch(Exception e)
+            {
+                Debug.LogError("You may need to exit out of any file explorer that's opened, otherwise the following error might be more specific");
+                Debug.LogError("Error: " + e);
+            }
+            
+        } else {
+            Debug.Log("Unable to create folder directory");
         }
+
         AssetDatabase.Refresh();
         
 
         //build
-        try{
-            UnityEngine.Debug.Log("Starting to build.");
+        try
+        {
+            Debug.Log("Starting to build.");
             StartBuild();
+            Debug.Log("Build successful.");
         } 
         catch (Exception e)
         {
             //undo asset changes            
-            UnityEngine.Debug.Log("Failed because something unexpected happened. Moving all files back. Error: " + e);            
+            Debug.Log("Failed because something unexpected happened. Error: " + e);            
         }
-        
-        
-        //after build 
-        UnityEngine.Debug.Log("Done building");
 
-        //move files
-        UnityEngine.Debug.Log("Moving files back");        
+        //move files back
+        Debug.Log("Moving files back");        
         AssetDatabase.Refresh();
-        //UnityEngine.Debug.Log("Moving back: " + AssetDatabase.MoveAsset(excludeFolderPath, photonDir + "/" + photonVoiceDir));
-        //Directory.Delete(photonDir + "/" + photonVoiceDir);  
         Directory.Move(excludeFolderPath, photonDir + "/" + photonVoiceDir);
 
         AssetDatabase.Refresh();
-        
-        
+
+        Debug.Log("Task Finished");        
     }
 
     static void StartBuild()
     {
+        if(CheckScenes.GetScenes().Count == 0)
+        {
+            throw new Exception("There are no scenes added");
+        }
         if(levels.Length != CheckScenes.GetScenes().Count)
         {
-            UnityEngine.Debug.LogError("Change the length of the levels (line 17) variable according to the scene count, currently: " + CheckScenes.GetScenes().Count);
-            return;
+            Debug.Log("Resizing levels array to accomodate scene list");
+            Array.Resize<string>(ref levels, CheckScenes.GetScenes().Count);
         }
             
-
         // Get filename.
         string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
 
+        // Gather all the scenes
         for(int i = 0; i < CheckScenes.GetScenes().Count; i++)
         {
             levels[i] = CheckScenes.GetScenes()[i];
         }
 
         // Build player.
-        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.WebGL, BuildOptions.None);
-
-        // Copy a file from the project folder to the build folder, alongside the built game.
-        //FileUtil.CopyFileOrDirectory("Assets/Templates/Readme.txt", path + "Readme.txt");
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", buildTarget, buildOpt);        
 
         // Run the game (Process class from System.Diagnostics).
-        Process proc = new Process();
+        System.Diagnostics.Process proc = new System.Diagnostics.Process();
         proc.StartInfo.FileName = path + "/BuiltGameWebGL";
         proc.Start();
     }

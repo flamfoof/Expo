b4a367db0c38eed0aa61b5c0682041481311bcac
Felix Lam <felixlam0316@gmail.com>
2020-06-19 01:50:08 -0400

    - Forgot to save a script

--- Assets/Scripts/PhotonNetwork/UIScripts/UIControlsDemo.cs
+++ Assets/Scripts/PhotonNetwork/UIScripts/UIControlsDemo.cs
@@ -22,30 +22,7 @@ public class UIControlsDemo : MonoBehaviour
         }
     }
 
-    public GenderList.genders CheckGender()
-    {
-        for(int i = 0; i < genderList.Length; i++)
-        {
-            if(genderList[i].isOn)
-            {
-                switch(genderList[i].GetComponent<GenderList>().gender)
-                {
-                    case GenderList.genders.Male: 
-                        Debug.Log("Male");
-                        return GenderList.genders.Male;
-                    case GenderList.genders.Female: 
-                        Debug.Log("Female");
-                        return GenderList.genders.Female;
-                    case GenderList.genders.NonBinary: 
-                        Debug.Log("Non-binary");
-                        return GenderList.genders.NonBinary;
-                    default: 
-                        break;
-                }
-            }            
-        }
-        return GenderList.genders.None;
-    }
+   
 
     //will be expanded upon
 }

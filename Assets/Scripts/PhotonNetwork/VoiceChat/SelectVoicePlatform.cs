a56f4b11f053aa98ffa4eb0f2ac5a60e34694453
Felix Lam <felixlam0316@gmail.com>
2020-06-23 18:02:42 -0400

    Photon test 1

---
+++ Assets/Scripts/PhotonNetwork/VoiceChat/SelectVoicePlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectVoicePlatform : MonoBehaviour
+{
+    public GameObject webglVoice;
+
+    void Start()
+    {
+        #if UNITY_WEBGL
+        webglVoice.SetActive(true);
+        #endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

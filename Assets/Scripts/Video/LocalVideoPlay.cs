715644cc2a93192866df1827d7672d845a1aac6e
Felix Lam <felixlam0316@gmail.com>
2020-07-06 18:58:32 -0400

    - Updated youtube audio playback

--- Assets/Scripts/Video/LocalVideoPlay.cs
+++ Assets/Scripts/Video/LocalVideoPlay.cs
@@ -6,6 +6,18 @@ public class LocalVideoPlay : MonoBehaviour
 {
     public bool isLocal;
     public bool isManual;
+    public bool isAudio = true;
+
+    AudioSource audio;
+
+    private void Start() {
+        audio = GetComponent<AudioSource>();
+        #if UNITY_WEBGL
+        if(!isAudio)
+            audio.enabled = false;
+            Debug.Log("Audio is off");
+        #endif
+    }
 
 
 }

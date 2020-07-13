60322a88ad5746294d6a63ec5b4666eb1085f554
Saptarshi Saha <saptarshi@redappletech.com>
2020-07-13 12:14:33 +0530

    Design The SignUp Page
    Design The SignUp Page

---
+++ Assets/Scripts/PhotonNetwork/UIScripts/SignUpController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUpController : MonoBehaviour
+{
+    [SerializeField] private InputField nameField;
+    [SerializeField] private InputField emailField;
+    [SerializeField] private InputField passwordField;
+    [SerializeField] private InputField confirmPasswordField;
+    [SerializeField] private Text feedbackTxt;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

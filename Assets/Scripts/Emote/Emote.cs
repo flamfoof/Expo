32acc5631e7237cdd10c9a8dd742fd6903f1007b
contrejo27 <contrejo27@gmail.com>
2020-07-23 15:54:52 -0700

    Added Emoji Materials.

--- Assets/Scripts/Emote/Emote.cs
+++ Assets/Scripts/Emote/Emote.cs
@@ -8,11 +8,28 @@ public class Emote : MonoBehaviourPunCallbacks
     public string name;
     public float duration = 5.0f;
 
+    Rigidbody rb;
+
     void Start()
-    {        
+    {
         Destroy(gameObject, duration);
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        StartEmojiEffects();
     }
 
+    private void Update()
+    {
+        //rb.AddForce(Vector3.up);
+
+
+    }
+    void StartEmojiEffects()
+    {
+        float forceIntensity = Random.Range(.5f, 5f);
+        Vector3 forceDirection = new Vector3(Random.Range(-2f, 5f), 4f, Random.Range(-2f, 5f));
+        rb.AddForce(forceDirection * forceIntensity);
+    }
     private void OnDestroy() {
         PhotonNetwork.Destroy(photonView);
     }

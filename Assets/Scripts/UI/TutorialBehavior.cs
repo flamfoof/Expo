c14e001130af9ea8c5431cbfed85ccb4e2f3521e
contrejo27 <contrejo27@gmail.com>
2020-07-27 23:11:57 -0700

    created intro tutorial

---
+++ Assets/Scripts/UI/TutorialBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TutorialBehavior : MonoBehaviour
+{
+    public GameObject[] tutorialScreens;
+    bool waitingforscreen = false;
+    int currentScreenNum = 0;
+    GameObject currentScreen;
+
+    void Start()
+    {
+        currentScreen = tutorialScreens[currentScreenNum];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!waitingforscreen)
+        {
+            if (Input.GetKeyDown(KeyCode.W) && currentScreen.name == "wTutorialScreen")
+            {
+                waitingforscreen = true;
+                NextTutorialScreen();
+            }
+            if (Input.GetMouseButton(1) && currentScreen.name == "rightClickTutorialScreen")
+            {
+                waitingforscreen = true;
+                NextTutorialScreen();
+            }
+            if (Input.GetKeyDown(KeyCode.Return) && currentScreen.name == "chatTutorialScreen")
+            {
+                waitingforscreen = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void NextTutorialScreen()
+    {
+        tutorialScreens[currentScreenNum].SetActive(false);
+        currentScreenNum++;
+
+        StartCoroutine("WaitForNewUI");
+    }
+
+    IEnumerator WaitForNewUI()
+    {
+        yield return new WaitForSeconds(1f);
+        waitingforscreen = false;
+        currentScreen = tutorialScreens[currentScreenNum];
+        currentScreen.SetActive(true);
+
+    }
+}

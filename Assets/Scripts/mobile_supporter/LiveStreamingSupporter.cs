d6539755705104b79dbe5dbf90b452485b6cb1d4
Jae Sam Lee <ruler0910@hotmail.com>
2020-11-06 00:32:27 +0800

    mobile streaming click

---
+++ Assets/Scripts/mobile_supporter/LiveStreamingSupporter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveStreamingSupporter : MonoBehaviour
+{
+    public GameObject clickButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnEnable()
+    {
+#if UNITY_IOS || UNITY_ANDROID
+        clickButton.SetActive(true);
+#else
+        clickButton.SetActive(false);
+#endif
+    }
+
+    public void ButtonClicked()
+    {
+        if(this.transform.GetChild(0).gameObject.GetComponent<ISimpleLiveStream>() != null)
+        {
+            this.transform.GetChild(0).gameObject.GetComponent<ISimpleLiveStream>().Perform(UnityEngine.InputSystem.InputActionPhase.Started);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/mobile_supporter/PresentationSupporter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentationSupporter : MonoBehaviour
+{
+    public GameObject clickButton;
+
+    public void OnEnable()
+    {
+#if UNITY_IOS || UNITY_ANDROID
+        clickButton.SetActive(true);
+#else
+        clickButton.SetActive(false);
+#endif
+    }
+
+    public void ButtonClicked()
+    {
+        if (this.gameObject.GetComponent<ISimplePresentation>() != null)
+        {
+            this.gameObject.GetComponent<ISimplePresentation>().Perform(UnityEngine.InputSystem.InputActionPhase.Started);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

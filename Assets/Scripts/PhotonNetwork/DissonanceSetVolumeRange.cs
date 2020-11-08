1406a3b5c8411ac1a7cc48440f46f5a23c9da497
Felix Lam <felixlam0316@gmail.com>
2020-11-08 04:06:01 -0500

    - Added Dissonance properties needed on player prefab

---
+++ Assets/Scripts/PhotonNetwork/DissonanceSetVolumeRange.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissonanceSetVolumeRange : MonoBehaviour
+{
+    public SphereCollider sphereTrigger;
+    public GameObject playbackPrefab;
+
+    private void Start() 
+    {
+        AudioSource audioSource = playbackPrefab.GetComponent<AudioSource>();
+
+        sphereTrigger.radius = audioSource.maxDistance;
+    }
+}

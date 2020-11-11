65e72160cdf20e52ea89fea85d31bd7ee58c8cbf
Felix Lam <felixlam0316@gmail.com>
2020-11-10 21:14:43 -0500

    - Adjusted UI resolution - Lowered the Dissonance echo cancellation - Added an echo filter to volume output - Changed landscape build to Landscape left - Fixed chat blocking the user input

--- Assets/Scripts/UI/UIAudioBehavior.cs
+++ Assets/Scripts/UI/UIAudioBehavior.cs
@@ -1,18 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using JetBrains.Annotations;
+using Dissonance.Demo;
+using Dissonance;
+using Photon.Pun;
 
 public class UIAudioBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private IDissonancePlayer _player;
+    private VoicePlayerState _state;
+    private float _intensity;
+
+    DissonanceComms dissonanceComms;
+
     void Start()
     {
+        if(!player.GetComponent<PhotonView>().IsMine)
+        {
+            //gameObject.SetActive(false);
+        }
+
+       dissonanceComms = GameObject.FindObjectOfType<DissonanceComms>();
+
+       //_state = dissonanceComms.FindPlayer(dissonanceComms.LocalPlayerName);
+       _player = player.GetComponent<IDissonancePlayer>();
+
+       StartCoroutine(FindPlayerState());
         
     }
+
+    private IEnumerator FindPlayerState()
+    {
+        //Wait until player tracking has initialized
+        while (!_player.IsTracking)
+            yield return null;
+
+        //Now ask Dissonance for the object which represents the state of this player
+        //The loop is necessary in case Dissonance is still initializing this player into the network session
+        while (_state == null)
+        {
+            _state = FindObjectOfType<DissonanceComms>().FindPlayer(_player.PlayerId);
+            yield return null;
+        }
+    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        if(_state.IsSpeaking)
+        {
+            GetComponent<Image>().enabled = true;
+        } else {
+            GetComponent<Image>().enabled = false;
+        }
         
+
     }
 }

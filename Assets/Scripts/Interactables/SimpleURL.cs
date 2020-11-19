938074aa9d3eefb9273e5721c6e9d00d16983cc1
contrejo27 <contrejo27@gmail.com>
2020-11-19 18:43:30 -0500

    client updates

--- Assets/Scripts/Interactables/SimpleURL.cs
+++ Assets/Scripts/Interactables/SimpleURL.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class SimpleURL : MonoBehaviour
 {
+    public string url;
     // Start is called before the first frame update
-    void Start()
+    public void openURL()
     {
-        
+        Application.OpenURL(url);
     }
 
     // Update is called once per frame
